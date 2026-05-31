a9ac872568607860aa5d2588dcbc0d416787e4b1
Mikk Laur <mikklaur5@gmail.com>
2026-05-31 10:13:59 +0300

    Fix for loop range typo in exercise 96 description

--- exercises/098_for3.zig
+++ exercises/098_for3.zig
@@ -37,7 +37,7 @@
 //
 //     var n: u32 = 1;
 //
-//     // I want to print every number between 1 and 20 that is NOT
+//     // I want to print every number between 0 and 20 that is NOT
 //     // divisible by 3 or 5.
 //     while (n <= 20) : (n += 1) {
 //         // The '%' symbol is the "modulo" operator and it
